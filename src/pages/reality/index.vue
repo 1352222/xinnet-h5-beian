521881f0b6530f333c46f714325f5fd52361c6a4
dongning2018 <dongning666666@163.com>
2021-01-04 14:32:27 +0800

    变更接入checkPhone添加手机号参数

--- src/pages/reality/index.vue
+++ src/pages/reality/index.vue
@@ -25,7 +25,7 @@ export default {
       // this.setData({ orderCode, orderType }, () => {
       //   app.globalData.orderCode = orderCode
       //   // 备案类型为新增接入、变更接入和变更主体时，无网站信息，跳过输入手机号步骤
-      //   if (orderType === 'NEW_CHECK_IN' || orderType === 'CHANGE_CHECK_IN' || orderType === 'CHANGE_ORG' || orderType === 'NO_ORG_NEW_CHECK_IN') {
+      //   if (orderType === 'NEW_CHECK_IN' || orderType === 'CHANGE_ORG' || orderType === 'NO_ORG_NEW_CHECK_IN') {
       //     this.getUserData()
       //   } else {
       //     this.toLoginPage()
